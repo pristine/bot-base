bc55acd70da5f7d2661354c835ad8f9a5a358c42
EdwinJ0124 <35356243+EdwinJ0124@users.noreply.github.com>
2021-05-25 20:03:38 -0700

    initialize footsites monitor

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/EdwinJ0124/footsites/sites/footsites"
+import (
+	"github.com/EdwinJ0124/footsites/monitors/footsitesMonitor"
+	"github.com/EdwinJ0124/footsites/sites/footsites"
+)
 
 func main() {
+	footsitesMonitor.Initialize()
 	footsites.Initialize()
 }
